efb8f63b62948c7603da4a399b73dc8d5cb27105
hypulse <sjinsilval28@naver.com>
2023-07-20 14:45:19 +0900

    Design system design completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,60 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      primary: "rgb(var(--color-primary))",
+      secondary: "rgb(var(--color-secondary))",
+      cardColor: "rgb(var(--bg-card))",
+      borderColor: "rgb(var(--color-border))",
+      textColor: "rgb(var(--color-text))",
+      textSecondaryColor: "rgb(var(--color-text-secondary))",
+    },
+    screens: {
+      tablet: "640px",
+      // => @media (min-width: 640px) { ... }
+      desktop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+    },
+    fontSize: {
+      title: "2rem", // 섹션 제목, 본문 제목
+      subTitle: "1.75rem", // 카드 제목
+      extra: "1.25rem", // nav, 사이드 바
+      body: "1rem", // 본문, 버튼
+      caption: "0.875rem", // 본문 부가 설명
+      meta: "0.75rem", // 카드 부가 설명
+    },
+    spacing: {
+      0: "0",
+      buttonSpacingX: "1rem", // 버튼 가로 간격
+      buttonSpacingY: "0.5rem", // 버튼 세로 간격
+      tagSpacingX: "0.5rem", // 태그 가로 간격
+      tagSpacingY: "0.25rem", // 태그 세로 간격
+      cardPadding: "2rem", // 카드 패딩
+      containerPadding: "1rem", // 컨테이너 패딩
+      sectionSpacing: "4rem", // 섹션 간격 e.g. 카드와 카드 사이
+      elementSpacing: "1.5rem", // 요소 간격 e.g. 카드 내부 영역 사이
+      columnGap: "0.5rem", // 칼럼 간격 e.g. 카드 제목과 내용 사이
+      rowGap: "1rem", // 행 간격 e.g. 태그와 태그 사이
+    },
+    borderRadius: {
+      DEFAULT: "0.5rem",
+      small: "0.25rem",
+    },
+    fontWeight: {
+      light: 200,
+      base: 400,
+      bold: 600,
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ["Pretendard", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
+};
