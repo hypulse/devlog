64d7f129d452f9cdcc11d50738cf0ec13473fa60
hypulse <sjinsilval28@naver.com>
2023-07-21 13:17:38 +0900

    Tag components

---
+++ components/tags.tsx
@@ -0,0 +1,26 @@
+import { TagSchema } from "@/types/schema";
+import Link from "next/link";
+
+const Tag = ({ _id, name }: TagSchema) => {
+  return (
+    <Link href={`/articles?tag=${_id}`}>
+      <div className="border rounded px-tagSpacingX py-tagSpacingY border-borderColor text-meta text-secondary">
+        {name.toLocaleLowerCase()}
+      </div>
+    </Link>
+  );
+};
+
+const TagWrapper = ({ tags }: { tags: Array<TagSchema> }) => {
+  return (
+    <div className="flex flex-wrap items-center -m-tagSpacingY">
+      {tags.map((tag) => (
+        <div key={tag._id} className="m-tagSpacingY">
+          <Tag {...tag} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export { Tag, TagWrapper };
