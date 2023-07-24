2b8fef951e754afee47e15435194f2256801e3cb
hypulse <sjinsilval28@naver.com>
2023-07-24 17:15:23 +0900

    InputBase forwardRef

--- components/inputs.tsx
+++ components/inputs.tsx
@@ -1,15 +1,16 @@
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, forwardRef } from "react";
 
-const InputBase = ({
-  className,
-  ...props
-}: InputHTMLAttributes<HTMLInputElement>) => {
+const InputBase = forwardRef<
+  HTMLInputElement,
+  InputHTMLAttributes<HTMLInputElement>
+>(({ className, ...props }, ref) => {
   return (
     <input
+      ref={ref}
       className={`block w-full border-0 outline-none placeholder-textSecondaryColor ring-1 ring-inset ring-borderColor focus:ring-2 focus:ring-secondary focus:ring-inset ${className}`}
       {...props}
     />
   );
-};
+});
 
 export { InputBase };
