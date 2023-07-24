25d12f1d7fdd39a2d4d6b1bc783329e9e991aa86
hypulse <sjinsilval28@naver.com>
2023-07-24 16:14:23 +0900

    transition 전체 적용

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,9 @@
 "use client";
 
 import useTheme from "@/utils/app/hooks/useTheme";
-import {
-  MaterialSymbolsDarkMode,
-  MaterialSymbolsLightMode,
-  MdiMagnify,
-} from "./icons";
-import { InputBase } from "./inputs";
-import { ShowOnTablet } from "./layouts";
+import { MaterialSymbolsDarkMode, MaterialSymbolsLightMode } from "./icons";
+import { SearchToggle, ShowOnTablet } from "./layouts";
+import Link from "next/link";
 
 const ThemeSwitcher = () => {
   const { theme, toggleTheme } = useTheme();
@@ -15,7 +11,7 @@ const ThemeSwitcher = () => {
   return (
     <button
       onClick={toggleTheme}
-      className="rounded-full p-tagSpacingY hover:bg-borderColor app-transition"
+      className="rounded-full p-tagSpacingY hover:bg-borderColor"
     >
       <div
         className="relative"
@@ -25,14 +21,14 @@ const ThemeSwitcher = () => {
         }}
       >
         <MaterialSymbolsLightMode
-          className="absolute top-0 left-0 app-transition"
+          className="absolute top-0 left-0"
           style={{
             transform: theme === "light" ? "scale(1)" : "scale(0)",
             opacity: theme === "light" ? 1 : 0,
           }}
         />
         <MaterialSymbolsDarkMode
-          className="absolute top-0 left-0 app-transition"
+          className="absolute top-0 left-0"
           style={{
             transform: theme === "dark" ? "scale(1)" : "scale(0)",
             opacity: theme === "dark" ? 1 : 0,
@@ -43,39 +39,52 @@ const ThemeSwitcher = () => {
   );
 };
 
-const SearchToggle = () => {
-  return (
-    <ShowOnTablet>
-      <div className="flex items-center gap-x-columnGap">
-        <InputBase
-          type="search"
-          className="text-body px-buttonSpacingX py-tagSpacingY bg-bgColor"
-          placeholder="Search..."
-        />
-        <button className="rounded-full p-tagSpacingY hover:bg-borderColor app-transition">
-          <MdiMagnify />
-        </button>
-      </div>
-    </ShowOnTablet>
-  );
-};
-
 const Header = () => {
+  const headerHeight = "3rem";
+
   return (
-    <header
-      className="w-full border-b border-borderColor mb-sectionSpacing bg-cardColor"
-      style={{
-        height: "3rem",
-      }}
-    >
-      <div className="flex items-center h-full mx-auto max-w-screen-desktop px-containerPadding text-extra">
-        <div className="flex-grow"></div>
-        <div className="flex items-center gap-x-columnGap">
-          <SearchToggle />
-          <ThemeSwitcher />
+    <div className="text-extra">
+      <div
+        className="mb-sectionSpacing"
+        style={{
+          height: headerHeight,
+        }}
+      />
+      <header
+        className="fixed top-0 left-0 z-10 w-full border-b border-borderColor bg-cardColor"
+        style={{
+          height: headerHeight,
+        }}
+      >
+        <div className="flex items-center h-full mx-auto max-w-screen-desktop px-containerPadding">
+          <div className="flex-grow">
+            <Link
+              href={`/`}
+              className="block overflow-hidden rounded-full"
+              style={{
+                width: "2rem",
+                height: "2rem",
+              }}
+            >
+              <div
+                className="w-full h-full bg-center bg-cover"
+                style={{
+                  backgroundImage: `url(${"https://avatars.githubusercontent.com/u/90980422?v=4"})`,
+                }}
+              />
+            </Link>
+          </div>
+          <div className="flex items-center gap-x-columnGap">
+            <ShowOnTablet>
+              <div className="max-w-xs">
+                <SearchToggle />
+              </div>
+            </ShowOnTablet>
+            <ThemeSwitcher />
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </div>
   );
 };
 
--- components/cards.tsx
+++ components/cards.tsx
@@ -15,7 +15,7 @@ const ArticleCard = ({
       <div className="block border p-cardPadding bg-cardColor border-borderColor group tablet:flex tablet:gap-x-elementSpacing tablet:items-center">
         <div className="overflow-hidden rounded aspect-video tablet:aspect-square basis-1/4 grow mb-elementSpacing tablet:mb-0">
           <div
-            className="w-full h-full scale-100 bg-center bg-cover group-hover:scale-125 app-transition"
+            className="w-full h-full scale-100 bg-center bg-cover group-hover:scale-125"
             style={{
               backgroundImage: `url(${
                 thumbnailImage || "/images/placeholder.png"
@@ -25,7 +25,7 @@ const ArticleCard = ({
         </div>
         <div className="basis-3/4 grow">
           <div className="mb-elementSpacing">
-            <h2 className="font-bold text-subTitle mb-columnGap group-hover:text-primary app-transition">
+            <h2 className="font-bold text-subTitle mb-columnGap group-hover:text-primary">
               {title}
             </h2>
             <p className="text-caption text-textSecondaryColor line-clamp-3">
@@ -66,7 +66,7 @@ const ArticleCardSmall = ({
           />
         </div>
         <div className="basis-2/3 grow">
-          <h2 className="font-bold text-body group-hover:text-primary app-transition">
+          <h2 className="font-bold text-body group-hover:text-primary">
             {title}
           </h2>
         </div>
