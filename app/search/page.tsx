85a349dd94b6861d502b903db887ce54a131ce04
hypulse <sjinsilval28@naver.com>
2023-07-26 02:23:50 +0900

    Search Page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,9 +1,20 @@
 import Search from "@/components/Search";
+import SmallCard from "@/components/SmallCard";
+import { cardDataDummy } from "@/utils/app/dummy";
 
 export default function Page() {
   return (
     <main className="mx-auto max-w-screen-desktop px-containerPadding space-y-sectionSpacing">
       <Search />
+      <div>
+        <div className="mb-elementSpacing text-caption text-textSecondaryColor">
+          <span className="text-primary">2</span> results
+        </div>
+        <div className="grid grid-cols-1 gap-y-columnGap gap-x-rowGap tablet:grid-cols-2">
+          <SmallCard {...cardDataDummy} />
+          <SmallCard {...cardDataDummy} />
+        </div>
+      </div>
     </main>
   );
 }
