0ff9b28c6b3bfbe848c4d4af98df7fe1a0bc49a3
hypulse <sjinsilval28@naver.com>
2023-07-27 13:18:59 +0900

    디자인 시스템 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,30 +18,30 @@ module.exports = {
       transparent: "transparent",
     },
     screens: {
-      tablet: "640px",
-      desktop: "1280px",
+      tablet: "600px",
+      desktop: "1024px",
     },
     fontSize: {
-      title: "2.5rem", // 본문 제목
-      subTitle: "1.75rem", // 카드 제목
-      extra: "1.25rem", // nav, 사이드 바
-      body: "1rem", // 본문, 버튼
-      caption: "0.875rem", // 본문 부가 설명
-      meta: "0.75rem", // 카드 부가 설명
+      title: "2rem", // H1
+      subTitle: "1.5rem", // H2
+      extra: "1.25rem", // H3
+      body: "1rem", // Body
+      caption: "0.875rem", // Caption
+      meta: "0.75rem", // Meta
     },
     spacing: {
       0: "0",
       buttonPaddingX: "1rem", // 버튼 X 패딩
-      buttonPaddingY: "0.5rem", // 버튼 Y 패딩
-      tagPaddingX: "0.5rem", // 태그 X 패딩
+      buttonPaddingY: "0.5625rem", // 버튼 Y 패딩
+      tagPaddingX: "0.75rem", // 태그 X 패딩
       tagPaddingY: "0.25rem", // 태그 Y 패딩
-      cardPadding: "2rem", // 카드 패딩
-      containerPadding: "1rem", // 컨테이너 패딩
-      sectionSpacing: "4rem", // 섹션 간격 e.g. 카드와 카드 사이
-      extraSpacing: "3rem", // 본문과 본문 사이
+      cardPadding: "1rem", // 카드 패딩
+      containerPadding: "1.5rem", // 컨테이너 패딩
+      sectionSpacing: "2rem", // 섹션 간격 e.g. 카드와 카드 사이
+      extraSpacing: "2rem", // 본문과 본문 사이
       elementSpacing: "1.5rem", // 요소 간격 e.g. 카드 내부 영역 사이
-      columnGap: "0.5rem", // 칼럼 간격 e.g. 카드 제목과 내용 사이
-      rowGap: "1rem", // 행 간격 e.g. 태그와 태그 사이
+      columnGap: "1rem", // 칼럼 간격 e.g. 카드 제목과 내용 사이
+      rowGap: "0.75rem", // 행 간격 e.g. 태그와 태그 사이
     },
     borderRadius: {
       DEFAULT: "0.5rem",
