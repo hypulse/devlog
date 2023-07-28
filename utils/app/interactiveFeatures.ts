53b5b7a9fc80cf781265a483087590ab99c3b1b0
hypulse <sjinsilval28@naver.com>
2023-07-28 11:22:46 +0900

    utils 메서드명 및 기능 개편

---
+++ utils/app/interactiveFeatures.ts
@@ -0,0 +1,45 @@
+async function copyToClipboard(text: string): Promise<void> {
+  if (navigator.clipboard) {
+    try {
+      await navigator.clipboard.writeText(text);
+      console.log("Text copied to clipboard");
+    } catch (err) {
+      console.error("Failed to copy text: ", err);
+    }
+  } else {
+    // Fallback for browsers that do not support the Clipboard API
+    let textArea = document.createElement("textarea");
+    textArea.value = text;
+    textArea.style.position = "fixed"; //avoid scrolling to bottom
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+      let successful = document.execCommand("copy");
+      let msg = successful ? "successful" : "unsuccessful";
+      console.log("Fallback: Copying text command was " + msg);
+    } catch (err) {
+      console.error("Fallback: Oops, unable to copy", err);
+    }
+
+    document.body.removeChild(textArea);
+  }
+}
+
+type ModifiedShareData = Omit<ShareData, "files"> & { text: string };
+
+async function shareData(data: ModifiedShareData): Promise<void> {
+  if (navigator.share) {
+    try {
+      await navigator.share(data);
+      console.log("Data shared successfully");
+    } catch (err) {
+      console.error("Failed to share: ", err);
+    }
+  } else {
+    await copyToClipboard(data.text);
+  }
+}
+
+export { copyToClipboard, shareData };
