b65c18b270d0f558d30fe405b2c3404d624654ab
hypulse <sjinsilval28@naver.com>
2023-07-31 13:16:59 +0900

    refactor copyToClipboard

--- utils/app/interactiveFeatures.ts
+++ utils/app/interactiveFeatures.ts
@@ -2,27 +2,22 @@ async function copyToClipboard(text: string): Promise<void> {
   if (navigator.clipboard) {
     try {
       await navigator.clipboard.writeText(text);
-      console.log("Text copied to clipboard");
+      return;
     } catch (err) {
-      console.error("Failed to copy text: ", err);
+      console.error("Failed to copy with navigator.clipboard:", err);
     }
-  } else {
-    // Fallback for browsers that do not support the Clipboard API
-    let textArea = document.createElement("textarea");
-    textArea.value = text;
-    textArea.style.position = "fixed"; //avoid scrolling to bottom
-    document.body.appendChild(textArea);
-    textArea.focus();
-    textArea.select();
+  }
 
-    try {
-      let successful = document.execCommand("copy");
-      let msg = successful ? "successful" : "unsuccessful";
-      console.log("Fallback: Copying text command was " + msg);
-    } catch (err) {
-      console.error("Fallback: Oops, unable to copy", err);
-    }
-
+  const textArea = document.createElement("textarea");
+  textArea.value = text;
+  textArea.style.position = "fixed";
+  document.body.appendChild(textArea);
+  textArea.select();
+  try {
+    document.execCommand("copy");
+  } catch (err) {
+    console.error("Failed to copy with execCommand:", err);
+  } finally {
     document.body.removeChild(textArea);
   }
 }
