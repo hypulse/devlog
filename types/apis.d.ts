e01d699235264a5e126feabafab3a6c37dcd0d06
hypulse <sjinsilval28@naver.com>
2023-08-01 15:45:24 +0900

    API 정의서

--- types/apis.d.ts
+++ types/apis.d.ts
@@ -1,6 +1,14 @@
 import { CardData } from "./data";
 import { ArticleSchema } from "./schema";
 
+type IdList = Array<string>;
+
+type DefaultResponse = {
+  success: true | string;
+};
+
+type HandleError = ({ success }: DefaultResponse) => void;
+
 type GetCards = ({
   page,
   limit,
@@ -13,7 +21,15 @@ type GetCards = ({
   admin?: boolean;
   type?: "article" | "snippet";
   status?: "draft" | "published" | "deleted";
-}) => Promise<Array<CardData & { maxIndex: number }>>;
+}) => Promise<
+  Array<
+    CardData & {
+      maxIndex: number;
+      page: number;
+      limit: number;
+    } & DefaultResponse
+  >
+>;
 
 type GetArticle = ({
   _id,
@@ -21,13 +37,59 @@ type GetArticle = ({
 }: {
   id: string;
   admin?: boolean;
-}) => Promise<ArticleSchema>;
+}) => Promise<ArticleSchema & DefaultResponse>;
 
-type GetPrevAndNext = ({ _id }) => Promise<{
-  prev: ArticleSchema | null;
-  next: ArticleSchema | null;
-}>;
+type GetPrevAndNext = (_id: string) => Promise<
+  {
+    prev: ArticleSchema | null;
+    next: ArticleSchema | null;
+  } & DefaultResponse
+>;
 
 type SearchArticles = (
   query: { q: string } | { tag: string }
-) => Promise<Array<ArticleSchema>>;
+) => Promise<Array<ArticleSchema> & DefaultResponse>;
+
+type Login = ({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) => Promise<DefaultResponse>;
+
+type Logout = () => Promise<DefaultResponse>;
+
+type CreateArticle = (payload: {
+  title: string;
+  content?: string;
+  description?: string;
+  thumbnailImage?: string;
+  wordCount: number;
+  tags?: IdList;
+  type: "article" | "snippet";
+  status: "draft" | "published" | "deleted";
+}) => Promise<DefaultResponse>;
+
+type UpdateArticle = (payload: {
+  _id: string;
+  title?: string;
+  content?: string;
+  description?: string;
+  thumbnailImage?: string;
+  wordCount?: number;
+  tags?: IdList;
+  type?: "article" | "snippet";
+  status?: "draft" | "published" | "deleted";
+}) => Promise<DefaultResponse>;
+
+type DeleteArticle = (payload: IdList) => Promise<DefaultResponse>;
+
+type CreateTag = (payload: IdList) => Promise<DefaultResponse>;
+
+type UpdateTag = (payload: {
+  _id: string;
+  name: string;
+}) => Promise<DefaultResponse>;
+
+type DeleteTag = (payload: IdList) => Promise<DefaultResponse>;
