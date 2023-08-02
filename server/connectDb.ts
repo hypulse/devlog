b90f02960a3d3b21bee023fd5d2a1bbf72dfd91e
hypulse <sjinsilval28@naver.com>
2023-08-02 17:47:39 +0900

    DB 연결

---
+++ server/connectDb.ts
@@ -0,0 +1,22 @@
+import mongoose, { ConnectOptions, Mongoose } from "mongoose";
+
+mongoose.set("strictQuery", false);
+
+async function connectDb(): Promise<Mongoose> {
+  console.log("Connecting to database...");
+  try {
+    const uri = process.env.DB_URI;
+    if (!uri) throw new Error("No database URI provided");
+    const db = await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as ConnectOptions);
+    console.log("Connected to database");
+    return db;
+  } catch (error) {
+    console.log("Error connecting to database:", error);
+    throw error;
+  }
+}
+
+export default connectDb;
