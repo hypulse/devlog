eaac68b242c6964ad2ff71c3d091816037b3840e
hypulse <sjinsilval28@naver.com>
2023-08-03 09:39:09 +0900

    Fix DB_URI name

--- connectDb.ts
+++ connectDb.ts
@@ -5,7 +5,7 @@ mongoose.set("strictQuery", false);
 async function connectDb(): Promise<Mongoose> {
   console.log("Connecting to database...");
   try {
-    const uri = process.env.NEXT_PUBLIC_DB_URI;
+    const uri = process.env.DB_URI;
     if (!uri) throw new Error("No database URI provided");
     const db = await mongoose.connect(uri, {
       useNewUrlParser: true,
