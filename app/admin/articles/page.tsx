1c291530db1453dd8950fd7d703a144f6dc7e789
hypulse <sjinsilval28@naver.com>
2023-08-06 23:45:55 +0900

    글 관리 페이지

--- app/admin/articles/page.tsx
+++ app/admin/articles/page.tsx
@@ -10,16 +10,37 @@ export default function Page() {
 
   return (
     <main className="flex flex-col mx-auto max-w-screen-desktop px-containerPadding space-y-sectionSpacing">
-      <div className="flex items-center justify-between">
-        <Pagination
-          totalPages={10}
-          currentPage={currentPage}
-          setCurrentPage={setCurrentPage}
-        />
+      <div className="flex items-center gap-x-columnGap">
+        <div>
+          <p className="text-meta text-textSecondaryColor mb-tagPaddingY">
+            Status
+          </p>
+          <select name="status" className="bg-transparent">
+            <option value="all">All</option>
+            <option value="draft">Draft</option>
+            <option value="published">Published</option>
+          </select>
+        </div>
+        <div>
+          <p className="text-meta text-textSecondaryColor mb-tagPaddingY">
+            Type
+          </p>
+          <select name="type" className="bg-transparent">
+            <option value="all">All</option>
+            <option value="article">Article</option>
+            <option value="snippet">Snippet</option>
+          </select>
+        </div>
+        <div className="flex-grow" />
         <Link href="/admin/articles/post">
           <Button>Post Article</Button>
         </Link>
       </div>
+      <Pagination
+        totalPages={10}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
     </main>
   );
 }
