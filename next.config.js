f95cab8f6f4c331d073b63d46e89cacca1812257
hypulse <sjinsilval28@naver.com>
2023-08-17 17:20:39 +0900

    config 초기화 및 신규 설정

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/posts",
-        destination: "/",
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,53 +8,53 @@ module.exports = {
   darkMode: "class",
   theme: {
     colors: {
-      bgColor: "rgba(var(--bg-color), <alpha-value>)",
-      cardColor: "rgba(var(--bg-card), <alpha-value>)",
-      textColor: "rgba(var(--color-text), <alpha-value>)",
-      textSecondaryColor: "rgba(var(--color-text-secondary), <alpha-value>)",
-      borderColor: "rgba(var(--color-border), <alpha-value>)",
-      primary: "rgba(var(--color-primary), <alpha-value>)",
-      secondary: "rgba(var(--color-secondary), <alpha-value>)",
+      background: "rgba(var(--background), <alpha-value>)",
+      card: "rgba(var(--card), <alpha-value>)",
+      text: "rgba(var(--text), <alpha-value>)",
+      textSecondary: "rgba(var(--text-secondary), <alpha-value>)",
+      border: "rgba(var(--border), <alpha-value>)",
+      primary: "rgba(var(--primary), <alpha-value>)",
+      secondary: "rgba(var(--secondary), <alpha-value>)",
+      white: "#ffffff",
+      black: "#000000",
       transparent: "transparent",
-      successColor: "rgba(var(--color-success), <alpha-value>)",
-      errorColor: "rgba(var(--color-error), <alpha-value>)",
     },
     screens: {
       tablet: "600px",
       desktop: "1024px",
     },
     fontSize: {
-      title: "2rem", // H1
-      subTitle: "1.5rem", // H2
-      extra: "1.25rem", // H3
-      body: "1rem", // Body
-      caption: "0.875rem", // Caption
-      meta: "0.75rem", // Meta
+      h1: "2rem",
+      h2: "1.5rem",
+      h3: "1.25rem",
+      base: "1rem",
+      caption: "0.875rem",
+      xs: "0.75rem",
     },
     spacing: {
       0: "0",
-      buttonPaddingX: "1rem", // 버튼 X 패딩
-      buttonPaddingY: "0.5625rem", // 버튼 Y 패딩
-      tagPaddingX: "0.75rem", // 태그 X 패딩
-      tagPaddingY: "0.25rem", // 태그 Y 패딩
-      cardPadding: "1rem", // 카드 패딩
-      containerPadding: "1.5rem", // 컨테이너 패딩
-      sectionSpacing: "4rem", // 섹션 간격 e.g. 카드와 카드 사이
-      extraSpacing: "2rem", // 본문과 본문 사이
-      elementSpacing: "1.5rem", // 요소 간격 e.g. 카드 내부 영역 사이
-      rowSpacing: "1rem", // 행 간격 e.g. 본문에서 제목과 내용 사이
-      rowGap: "0.75rem", // 행 간격 e.g. 카드 제목과 내용 사이
-      columnGap: "1rem", // 칼럼 간격 e.g. 태그와 태그 사이
+      inputPadding: "0.375rem",
+      buttonPaddingX: "0.8rem",
+      buttonPaddingY: "0.4rem",
+      tagPaddingX: "0.6rem",
+      tagPaddingY: "0.2rem",
+      cardPadding: "1rem",
+      containerPadding: "1.5rem",
+      sectionGap: "4rem",
+      extraGap: "2rem",
+      elementGap: "1.5rem",
+      rowGap: "0.5rem",
+      colGap: "0.75rem",
     },
     borderRadius: {
       DEFAULT: "0.5rem",
-      small: "0.25rem",
+      sm: "0.375rem",
       full: "9999px",
     },
     fontWeight: {
-      light: 200,
-      base: 400,
-      bold: 600,
+      light: "300",
+      base: "400",
+      bold: "700",
     },
   },
   plugins: [],
