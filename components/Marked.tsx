08e3eef7b6e5e480c0d2e82c294e8e7ad9bd3c4f
hypulse <sjinsilval28@naver.com>
2023-08-19 19:55:39 +0900

    Edit arked code style, remove spaces of html template

--- components/Marked.tsx
+++ components/Marked.tsx
@@ -12,7 +12,17 @@ interface MarkedProps extends HTMLAttributes<HTMLDivElement> {
 const renderer: marked.RendererObject = {
   code(code, lang) {
     lang = lang || "plaintext";
-    return `<div class="code-block"><div class="code-block-lang">${lang}</div><button class="code-block-copy">Copy</button><pre><code class="hljs language-${lang}">${code}</code></pre></div>`;
+    return `<div class="code-block">
+    <div class="code-block-lang">${lang}</div>
+    <button class="code-block-copy">Copy</button>
+    <pre>
+      <code class="hljs language-${lang}">{code}</code>
+    </pre>
+  </div>`
+      .replace(/\s+</g, "<")
+      .replace(/>[\s\n]+</g, "><")
+      .trim()
+      .replace("{code}", code);
   },
   codespan(code) {
     return `<code class="hljs">${code}</code>`;
