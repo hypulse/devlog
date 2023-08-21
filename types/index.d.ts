4fafd6c4305869fef0e3a587871c8b171d359b46
hypulse <sjinsilval28@naver.com>
2023-08-21 15:29:40 +0900

    Client Type of the postSchema

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,12 @@
 export type PostState = "active" | "removed" | "draft";
+
+export type PostType = {
+  _id: string;
+  title: string;
+  summary: string;
+  wordCount: number;
+  content: string;
+  state: PostState;
+  createdAt: Date;
+  updatedAt: Date;
+};
