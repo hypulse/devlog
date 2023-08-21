7a286454969eeccf459f80eb6dc29d8bda2a12ff
hypulse <sjinsilval28@naver.com>
2023-08-21 17:56:14 +0900

    Select box to edit state

--- app/admin/posts/[id]/page.tsx
+++ app/admin/posts/[id]/page.tsx
@@ -8,6 +8,7 @@ export default function Page() {
   const id = query.id as string | undefined;
 
   const [markdownContent, setMarkdownContent] = useState<string>("");
+  const [state, setState] = useState<PostState>("draft");
 
   useEffect(() => {
     if (isReady && id) {
@@ -18,6 +19,8 @@ export default function Page() {
   const fetchPost = async (postId: string) => {
     const fetchedPost = await getPost(postId);
     setMarkdownContent(fetchedPost.content || "");
+    setState(fetchedPost.state);
+  };
   };
 
   const handleSubmit = async () => {
@@ -46,6 +49,13 @@ export default function Page() {
         onChange={(e) => setMarkdownContent(e.target.value)}
         placeholder="Write your markdown content here..."
       />
+      <select
+        value={state}
+        onChange={(e) => setState(e.target.value as PostState)}
+      >
+        <option value="draft">Draft</option>
+        <option value="active">Publish</option>
+      </select>
       <button onClick={handleSubmit}>{id ? "Update" : "Create"}</button>
     </div>
   );
