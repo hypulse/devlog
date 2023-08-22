b3e760b8c9342f3a971f9700ef8d727c121d9027
hypulse <sjinsilval28@naver.com>
2023-08-22 11:02:09 +0900

    Infinite scroll post card list

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,64 @@
+import { useState, useEffect, useRef } from "react";
 import Card from "@/components/Card";
 import SearchBox from "@/components/SearchBox";
+import { PostTypeGet } from "@/types";
+import { getPosts } from "@/utils/apis/posts";
 
 export default function Page() {
+  const [posts, setPosts] = useState<Array<PostTypeGet>>([]);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+
+  const observer = useRef<IntersectionObserver | null>(null);
+  const lastPostElementRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const loadMorePosts = async () => {
+      const newPosts = await getPosts("active", page);
+
+      if (newPosts.length === 0) {
+        setHasMore(false);
+        return;
+      }
+      setPosts((prevPosts) => [...prevPosts, ...newPosts]);
+    };
+
+    if (hasMore) {
+      loadMorePosts();
+    }
+  }, [page]);
+
+  useEffect(() => {
+    if (observer.current) observer.current.disconnect();
+
+    const callback: IntersectionObserverCallback = (entries) => {
+      if (entries[0].isIntersecting && hasMore) {
+        setPage((prevPage) => prevPage + 1);
+      }
+    };
+
+    observer.current = new IntersectionObserver(callback);
+    if (lastPostElementRef.current) {
+      observer.current.observe(lastPostElementRef.current);
+    }
+
+    return () => {
+      if (observer.current) observer.current.disconnect();
+    };
+  }, [posts, hasMore]);
+
   return (
     <div className="space-y-sectionGap">
       <SearchBox />
       <div className="space-y-extraGap">
-        <Card _id="1" />
-        <Card _id="2" />
+        {posts.map((post, index) => (
+          <Card key={index} {...post} />
+        ))}
+        {hasMore && (
+          <div ref={lastPostElementRef} className="loading">
+            Loading...
+          </div>
+        )}
       </div>
     </div>
   );
