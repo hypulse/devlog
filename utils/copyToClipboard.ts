a4214fabd1b0c67a19d3e34bb22d244afc032dec
hypulse <sjinsilval28@naver.com>
2023-08-22 11:39:57 +0900

    copyToClipboard utils

---
+++ utils/copyToClipboard.ts
@@ -0,0 +1,9 @@
+export default function copyToClipboard(text: string) {
+  const textarea = document.createElement("textarea");
+  textarea.value = text;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textarea);
+  console.log("URL copied to clipboard!");
+}
