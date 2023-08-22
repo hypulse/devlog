39b71291fc0b6a13b135da6189a33f6c8fe8700d
hypulse <sjinsilval28@naver.com>
2023-08-22 11:48:50 +0900

    Add the sharePost clickHandler

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -2,24 +2,7 @@ import Marked from "@/components/Marked";
 import { getPost } from "@/utils/apis/posts";
 import { PostTypeGet } from "@/types";
 import { GetServerSideProps } from "next";
-
-export default function Page({ data }: { data: PostTypeGet }) {
-  const { _id, title, createdAt, summary, content } = data;
-  return (
-    <div className="max-w-2xl mx-auto">
-      <h1 className="font-bold text-h1">{title}</h1>
-      <div className="flex mt-elementGap gap-x-colGap">
-        <time dateTime={createdAt.toISOString()}>
-          {createdAt.toLocaleTimeString()}
-        </time>
-        <span>&middot;</span>
-        <button>Share</button>
-      </div>
-      <p className="mt-elementGap text-primary">{summary}</p>
-      <Marked text={content || ""} className="mt-extraGap" />
-    </div>
-  );
-}
+import copyToClipboard from "@/utils/copyToClipboard";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const postId = context.params?.id;
@@ -44,3 +27,38 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     },
   };
 };
+
+export default function Page({ data }: { data: PostTypeGet }) {
+  const { title, createdAt, summary, content } = data;
+
+  const sharePost = async () => {
+    if (navigator.share) {
+      try {
+        await navigator.share({
+          title: title,
+          text: summary,
+          url: window.location.href,
+        });
+        return;
+      } catch (error) {
+        console.error("Error sharing:", error);
+      }
+    }
+    copyToClipboard(window.location.href);
+  };
+
+  return (
+    <div className="max-w-2xl mx-auto">
+      <h1 className="font-bold text-h1">{title}</h1>
+      <div className="flex mt-elementGap gap-x-colGap">
+        <time dateTime={createdAt.toISOString()}>
+          {createdAt.toLocaleTimeString()}
+        </time>
+        <span>&middot;</span>
+        <button onClick={sharePost}>Share</button>
+      </div>
+      <p className="mt-elementGap text-primary">{summary}</p>
+      <Marked text={content || ""} className="mt-extraGap" />
+    </div>
+  );
+}
