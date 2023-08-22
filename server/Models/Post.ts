45479f7941e41afa3c22d03aa7f6f0212fb59471
hypulse <sjinsilval28@naver.com>
2023-08-22 15:49:47 +0900

    Models

---
+++ server/Models/Post.ts
@@ -0,0 +1,44 @@
+import { PostState } from "@/types/post";
+import { Document, Schema, model, models } from "mongoose";
+
+interface PostDocument extends Document {
+  title: string;
+  summary: string;
+  wordCount: number;
+  content: string;
+  state: PostState;
+}
+
+const postSchema = new Schema<PostDocument>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    summary: {
+      type: String,
+      required: false,
+    },
+    wordCount: {
+      type: Number,
+      required: false,
+    },
+    content: {
+      type: String,
+      required: false,
+    },
+    state: {
+      type: String,
+      enum: ["active", "removed", "draft"],
+      default: "draft",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Post = models.Post || model<PostDocument>("Post", postSchema);
+
+export default Post;
---
+++ server/Models/User.ts
@@ -0,0 +1,54 @@
+import { CallbackError, Document, Schema, model, models } from "mongoose";
+import bcrypt from "bcrypt";
+
+const SALT_ROUNDS = 10;
+
+interface UserDocument extends Document {
+  email: string;
+  password: string;
+}
+
+type LoginParams = {
+  email: string;
+  password: string;
+};
+
+const userSchema = new Schema<UserDocument>({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(SALT_ROUNDS);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error as CallbackError);
+  }
+});
+
+userSchema.statics.login = async function ({ email, password }: LoginParams) {
+  const user = await this.findOne({ email });
+  if (!user) throw new Error("No user associated with this email address.");
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) throw new Error("Incorrect password.");
+
+  return user;
+};
+
+const User = models.User || model("User", userSchema);
+
+module.exports = User;
