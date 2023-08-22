3586545eb4d2b41f965376e82cbed751ab075301
hypulse <sjinsilval28@naver.com>
2023-08-22 17:55:44 +0900

    Post/[id] api handlers

---
+++ pages/api/posts/[id].ts
@@ -0,0 +1,65 @@
+import Post from "@/server/Models/Post";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const {
+    query: { id },
+    method,
+  } = req;
+
+  switch (method) {
+    case "GET":
+      try {
+        const post = await Post.findById(id);
+        if (!post) {
+          return res
+            .status(404)
+            .json({ error: true, message: "Post not found" });
+        }
+        res.status(200).json({ error: false, data: post });
+      } catch (error) {
+        res
+          .status(400)
+          .json({ error: true, message: "Failed to retrieve post" });
+      }
+      break;
+
+    case "PATCH":
+      try {
+        const post = await Post.findByIdAndUpdate(id, req.body, {
+          new: true,
+          runValidators: true,
+        });
+        if (!post) {
+          return res
+            .status(404)
+            .json({ error: true, message: "Post not found" });
+        }
+        res.status(200).json({ error: false, data: post });
+      } catch (error) {
+        res.status(400).json({ error: true, message: "Failed to update post" });
+      }
+      break;
+
+    case "DELETE":
+      try {
+        const deletedPost = await Post.deleteOne({ _id: id });
+        if (!deletedPost) {
+          return res
+            .status(404)
+            .json({ error: true, message: "Post not found" });
+        }
+        res.status(200).json({ error: false, data: {} });
+      } catch (error) {
+        res.status(400).json({ error: true, message: "Failed to delete post" });
+      }
+      break;
+
+    default:
+      res.status(400).json({ error: true, message: "Invalid request method" });
+      break;
+  }
+}
