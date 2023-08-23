d96602378bc8a619f9918d8063b6f66cee4970b8
hypulse <sjinsilval28@naver.com>
2023-08-23 11:29:09 +0900

    default 5 secs timeout

--- utils/fetchAPI.ts
+++ utils/fetchAPI.ts
@@ -4,8 +4,13 @@ export default async function fetchAPI<T>(
   path: string,
   options: RequestInit = {}
 ): Promise<APIResponse<T>> {
+  const controller = new AbortController();
+  const { signal } = controller;
+
+  setTimeout(() => controller.abort(), 5000);
+
   try {
-    const response = await fetch(path, options);
+    const response = await fetch(path, { ...options, signal });
 
     if (response.ok) {
       const data = await response.json();
@@ -14,7 +19,16 @@ export default async function fetchAPI<T>(
       throw new Error(`Server returned a ${response.status} status.`);
     }
   } catch (err) {
-    const message = err instanceof Error ? err.message : "Unknown error";
+    let message: string;
+    if (err instanceof Error) {
+      if (err.name === "AbortError") {
+        message = "The request was aborted due to a timeout.";
+      } else {
+        message = err.message;
+      }
+    } else {
+      message = "Unknown error";
+    }
     return {
       error: true,
       message: `An error has occurred: ${message}`,
