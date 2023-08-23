171dd77585d19dab4f8910a74374c38ffcbe42ae
hypulse <sjinsilval28@naver.com>
2023-08-23 17:49:26 +0900

    POST LIST 테이블화

--- pages/admin/posts.tsx
+++ pages/admin/posts.tsx
@@ -33,7 +33,7 @@ export default function Page() {
   };
 
   const handleEdit = (id: string) => {
-    router.push(`/admin/posts/${id}`);
+    router.push(`/admin/editor?id=${id}`);
   };
 
   const handleDelete = async (id: string) => {
@@ -53,6 +53,7 @@ export default function Page() {
           <select
             value={state}
             onChange={(e) => setState(e.target.value as PostState)}
+            className="ml-xsGap"
           >
             <option value="active">Active</option>
             <option value="draft">Draft</option>
@@ -65,6 +66,7 @@ export default function Page() {
           <select
             value={limit}
             onChange={(e) => setLimit(parseInt(e.target.value, 10))}
+            className="ml-xsGap"
           >
             <option value={5}>5</option>
             <option value={10}>10</option>
@@ -74,14 +76,31 @@ export default function Page() {
         </label>
       </div>
 
-      {posts.map((post) => (
-        <div key={post._id}>
-          <h3>{post.title}</h3>
-          <button onClick={() => handlePublish(post._id)}>Publish</button>
-          <button onClick={() => handleEdit(post._id)}>Edit</button>
-          <button onClick={() => handleDelete(post._id)}>Delete</button>
-        </div>
-      ))}
+      <table className="min-w-full border-collapse">
+        <thead className="bg-card">
+          <tr className="text-left">
+            <th className="p-xsGap">Date</th>
+            <th className="p-xsGap">Title</th>
+            <th className="p-xsGap">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map((post) => (
+            <tr key={post._id} className="even:bg-card">
+              <td className="p-xsGap">
+                {new Date(post.createdAt).toLocaleString()}
+              </td>
+              <td className="p-xsGap">{post.title}</td>
+              <td className="p-xsGap flex gap-x-colGap">
+                <button onClick={() => handlePublish(post._id)}>Publish</button>
+                <button onClick={() => handleEdit(post._id)}>Edit</button>
+                <button onClick={() => handleDelete(post._id)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
       <div className="flex justify-center items-center gap-x-colGap">
         <button onClick={() => setPage((prev) => Math.max(prev - 1, 1))}>
           Previous
