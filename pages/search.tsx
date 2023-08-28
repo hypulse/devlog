66e1a0246a3140925b6a73cf9cb3f2bdebaab25b
hypulse <sjinsilval28@naver.com>
2023-08-29 00:12:39 +0900

    검색 페이지 작업 완료

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,3 +1,46 @@
+import Card from "@/components/Card";
+import SearchBox from "@/components/SearchBox";
+import { PostTypeGet } from "@/types/post";
+import { searchPosts } from "@/utils/apis/posts";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
 export default function Page() {
-  return null;
+  const router = useRouter();
+
+  const [posts, setPosts] = useState<Array<PostTypeGet>>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const q = router.query.q;
+      if (!q) return;
+
+      const { error, data } = await searchPosts(q as string, 1);
+
+      if (error || !data) {
+        setLoading(false);
+        return;
+      }
+      setPosts(data);
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [router]);
+
+  const renderPosts = () => {
+    if (loading) return <div>Loading...</div>;
+
+    if (posts.length === 0) return <div>No search results found.</div>;
+
+    return posts.map((post) => <Card key={post._id} {...post} />);
+  };
+
+  return (
+    <div className="space-y-sectionGap">
+      <SearchBox />
+      <div className="space-y-extraGap">{renderPosts()}</div>
+    </div>
+  );
 }
