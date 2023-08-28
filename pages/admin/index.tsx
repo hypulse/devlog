772c06e3546687e5c1323125f66dc6492a83156c
hypulse <sjinsilval28@naver.com>
2023-08-28 15:16:54 +0900

    조건부 redirect

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,5 +1,19 @@
-// login users will be routed to /admin/editor
-// non-login users will be routed to /admin/login
+export const getServerSideProps = async () => {
+  if (false) {
+    return {
+      redirect: {
+        destination: "/admin/login",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    redirect: {
+      destination: "/admin/posts",
+      permanent: false,
+    },
+  };
+};
 
 export default function Page() {
   return null;
