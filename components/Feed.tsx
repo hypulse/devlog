70903ff23bc1e8843463860e56ba9ea782a31598
hypulse <sjinsilval28@naver.com>
2023-08-30 15:01:13 +0900

    피드 작업 중

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,82 +1,84 @@
-const FEED: React.FC<FeedProps> = ({
-  _id,
-  title,
-  summary,
-  wordCount,
-  content,
-  state,
-  createdAt,
-  updatedAt,
-}) => {
+import { PostTypeGet } from "@/types/post";
+
+export default function Feed({ _id, title, createdAt, content }: PostTypeGet) {
+  const { paragraph, code } = extractFirstParagraphAndCode(content);
+
+  return null;
+  // <div className="p-cardPadding pb-0 roundd-sm bg-card shadow">
+  //   <div className="flex justify-between items-center text-h2">
+  //     <div className="font-bold">{title}</div>
+  //   </div>
+  //   <div className="mt-rowGap">{paragraph}</div>
+  //   <div className="mt-gap">
+  //     <Marked text={code || ""} />
+  //   </div>
+
+  //   <div className="text-textSecondary text-caption py-gap justify-between flex items-center gap-x-colGap">
+  //     <div className="text-caption">
+  //       {new Date(createdAt).toLocaleString()}
+  //     </div>
+  //     <span>edit</span>
+  //   </div>
+
+  //   <div className="flex items-center border-t border-border">
+  //     <button className="p-gap flex items-center justify-center grow gap-x-xsGap">
+  //       <RiChat3Fill />
+  //       <span className="text-caption">Comment</span>
+  //     </button>
+  //     <button className="p-gap flex items-center justify-center grow gap-x-xsGap">
+  //       <RiShareForwardFill />
+  //       <span className="text-caption">Share</span>
+  //     </button>
+  //   </div>
+  // </div>
+}
+
+function extractFirstParagraphAndCode(markdownString: string = "") {
+  const paragraphRegex = /^>([^\n]+)/m;
+  const codeRegex = /(```[\s\S]*?```)/;
+
+  const paragraphMatch = markdownString.match(paragraphRegex);
+  const codeMatch = markdownString.match(codeRegex);
+
+  const firstParagraph = paragraphMatch ? paragraphMatch[1].trim() : null;
+  const firstCode = codeMatch ? codeMatch[1].trim() : null;
+
+  return {
+    paragraph: firstParagraph,
+    code: firstCode,
+  };
+}
+
+function RiShareForwardFill(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <div
-      style={{
-        border: "1px solid #e1e4e8",
-        borderRadius: "10px",
-        padding: "16px",
-        transition: "background-color 0.3s",
-        cursor: "pointer",
-        marginBottom: "20px",
-        ":hover": {
-          backgroundColor: "#f6f8fa",
-        },
-      }}
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
     >
-      {/* Post Header */}
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <h2 style={{ fontSize: "18px", fontWeight: 600 }}>{title}</h2>
-        <span style={{ fontSize: "12px", color: "#586069" }}>
-          {new Date(createdAt).toLocaleDateString()}
-        </span>
-      </div>
-
-      {/* Content Preview */}
-      {summary && (
-        <p style={{ marginTop: "10px", color: "#586069" }}>{summary}</p>
-      )}
-      {content && (
-        <pre
-          style={{
-            marginTop: "10px",
-            backgroundColor: "#f6f8fa",
-            padding: "10px",
-            borderRadius: "5px",
-            overflowX: "auto",
-          }}
-        >
-          <code>{content}</code>
-        </pre>
-      )}
-
-      {/* Footer Info */}
-      <div
-        style={{
-          marginTop: "15px",
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          fontSize: "12px",
-          color: "#586069",
-        }}
-      >
-        {wordCount && <span>{wordCount} words</span>}
-        <div>
-          {state === "draft" && (
-            <span style={{ color: "#ffc107", marginRight: "10px" }}>Draft</span>
-          )}
-          {state === "published" && (
-            <span style={{ color: "#28a745" }}>Published</span>
-          )}
-        </div>
-      </div>
-    </div>
+      <path
+        fill="currentColor"
+        d="M13 14h-2a8.999 8.999 0 0 0-7.968 4.81A10.133 10.133 0 0 1 3 18C3 12.477 7.477 8 13 8V3l10 8l-10 8v-5Z"
+      ></path>
+    </svg>
   );
-};
+}
 
-export default FEED;
+function RiChat3Fill(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M7.291 20.824L2 22l1.176-5.291A9.956 9.956 0 0 1 2 12C2 6.477 6.477 2 12 2s10 4.477 10 10s-4.477 10-10 10a9.956 9.956 0 0 1-4.709-1.176Z"
+      ></path>
+    </svg>
+  );
+}
