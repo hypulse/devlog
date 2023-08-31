a4eb46d07f5e0c7ce1a3daa5db6d9a442f86fbda
hypulse <sjinsilval28@naver.com>
2023-08-31 18:13:45 +0900

    생성 버튼 추가

--- pages/admin/posts.tsx
+++ pages/admin/posts.tsx
@@ -5,6 +5,7 @@ import { getPosts, updatePostState } from "@/utils/apis/posts";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import DefaultButton from "@/components/Button";
 
 export default function Page() {
   const router = useRouter();
@@ -107,6 +108,16 @@ export default function Page() {
         </tbody>
       </table>
 
+      <div className="flex justify-end">
+        <DefaultButton
+          className="bg-primary flex items-center gap-x-xsGap"
+          onClick={() => router.push("/admin/editor")}
+        >
+          <RiEdit2Fill />
+          <span>New Post</span>
+        </DefaultButton>
+      </div>
+
       <Pagination page={page} setPage={setPage} />
     </div>
   );
@@ -139,6 +150,23 @@ function RiShareForward2Line(props: React.SVGProps<SVGSVGElement>) {
       <path
         fill="currentColor"
         d="M4 19h16v-5h2v6a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-6h2v5ZM16.172 7l-3.95-3.95l1.414-1.414L20 8l-6.364 6.364l-1.414-1.415L16.172 9H5V7h11.172Z"
+      ></path>
+    </svg>
+  );
+}
+
+function RiEdit2Fill(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M9.243 18.996H21v2H3v-4.242l9.9-9.9l4.242 4.243l-7.9 7.9Zm5.07-13.556l2.122-2.121a1 1 0 0 1 1.414 0l2.829 2.828a1 1 0 0 1 0 1.414l-2.122 2.122l-4.242-4.243Z"
       ></path>
     </svg>
   );
