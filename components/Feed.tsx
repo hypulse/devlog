5a3ef04039de47004e4ab21104da8f61db50eb69
hypulse <sjinsilval28@naver.com>
2023-09-01 14:12:44 +0900

    Feed 수정

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -30,9 +30,11 @@ export default function Feed({ _id, title, createdAt, content }: PostTypeGet) {
 
       {quote && <p>{quote}</p>}
 
-      <Link href={`/posts/${_id}`} className="text-primary">
-        Comment
-      </Link>
+      <div className="flex">
+        <Link href={`/posts/${_id}`} className="text-primary">
+          Comment
+        </Link>
+      </div>
     </div>
   );
 }
@@ -51,21 +53,4 @@ function extractFirstQuoteAndCode(markdown: string = "") {
     quote: extractedQuote,
     code: extractedCode,
   };
-}
-
-function RiChat3Fill(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="1em"
-      height="1em"
-      viewBox="0 0 24 24"
-      {...props}
-    >
-      <path
-        fill="currentColor"
-        d="M7.291 20.824L2 22l1.176-5.291A9.956 9.956 0 0 1 2 12C2 6.477 6.477 2 12 2s10 4.477 10 10s-4.477 10-10 10a9.956 9.956 0 0 1-4.709-1.176Z"
-      ></path>
-    </svg>
-  );
 }
