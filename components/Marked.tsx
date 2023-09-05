6f9297dea3386cddbf103c00ba68ca088e8d2f95
hypulse <sjinsilval28@naver.com>
2023-09-05 16:43:35 +0900

    global scope marked are used

--- components/Marked.tsx
+++ components/Marked.tsx
@@ -1,36 +1,51 @@
 import { HTMLAttributes } from "react";
-import { RendererObject } from "marked";
-import baseMarked from "@/utils/baseMarked";
+import { marked } from "marked";
 import { useEffect, useRef } from "react";
 import copyToClipboard from "@/utils/copyToClipboard";
+import { markedHighlight } from "marked-highlight";
+import hljs from "highlight.js";
+import { mangle } from "marked-mangle";
+import { gfmHeadingId } from "marked-gfm-heading-id";
 
 interface MarkedProps extends HTMLAttributes<HTMLDivElement> {
   text: string;
 }
 
-const renderer: RendererObject = {
-  code(code, lang) {
-    lang = lang || "plaintext";
-    return `<div class="code-block group">
-    <div class="code-block-lang">${lang}</div>
-    <button class="code-block-copy group-hover:opacity-100"></button>
-    <pre>
-      <code class="hljs language-${lang}">{code}</code>
-    </pre>
-  </div>`
-      .replace(/\s+</g, "<")
-      .replace(/>[\s\n]+</g, "><")
-      .trim()
-      .replace("{code}", code);
-  },
-  codespan(code) {
-    return `<code class="hljs">${code}</code>`;
-  },
-};
-
-baseMarked.use({
-  renderer,
+marked.setOptions({
+  gfm: true,
 });
+
+marked.use(
+  mangle(),
+  gfmHeadingId(),
+  markedHighlight({
+    highlight(code, lang) {
+      const language = hljs.getLanguage(lang) ? lang : "plaintext";
+      return hljs.highlight(code, { language }).value;
+    },
+  }),
+  {
+    renderer: {
+      code(code, lang) {
+        lang = lang || "plaintext";
+        return `<div class="code-block group">
+      <div class="code-block-lang">${lang}</div>
+      <button class="code-block-copy group-hover:opacity-100"></button>
+      <pre>
+        <code class="hljs language-${lang}">{code}</code>
+      </pre>
+    </div>`
+          .replace(/\s+</g, "<")
+          .replace(/>[\s\n]+</g, "><")
+          .trim()
+          .replace("{code}", code);
+      },
+      codespan(code) {
+        return `<code class="hljs">${code}</code>`;
+      },
+    },
+  }
+);
 
 const Marked = ({ text, ...props }: MarkedProps) => {
   const containerRef = useRef<HTMLDivElement | null>(null);
@@ -70,7 +85,7 @@ const Marked = ({ text, ...props }: MarkedProps) => {
   return (
     <div
       ref={containerRef}
-      dangerouslySetInnerHTML={{ __html: baseMarked(text) }}
+      dangerouslySetInnerHTML={{ __html: marked(text) }}
       {...props}
     />
   );
