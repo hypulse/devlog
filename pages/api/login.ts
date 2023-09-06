d22098277336cf397b58281e2fbfe67c3baaefff
hypulse <sjinsilval28@naver.com>
2023-09-06 14:58:36 +0900

    Login, Logout api 구현 완료

---
+++ pages/api/login.ts
@@ -0,0 +1,41 @@
+import User from "@/server/Models/User";
+import connectToDatabase from "@/server/connectToDatabase";
+import { NextApiRequest, NextApiResponse } from "next";
+import jwt from "jsonwebtoken";
+
+const EXPIRES_IN = 60 * 60;
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await connectToDatabase();
+
+  if (req.method === "POST") {
+    try {
+      const { email, password } = req.body;
+      const user = await User.login({ email, password });
+
+      const token = jwt.sign(
+        { id: user._id },
+        process.env.JWT_SECRET as string,
+        {
+          expiresIn: EXPIRES_IN,
+        }
+      );
+
+      res.setHeader(
+        "Set-Cookie",
+        `token=${token}; path=/; expires=${new Date(
+          Date.now() + EXPIRES_IN * 1000
+        )}; HttpOnly; Secure`
+      );
+
+      res.status(200).json({ error: false });
+    } catch (error) {
+      res.status(400).json({ error: true, message: "Authentication failed" });
+    }
+  } else {
+    res.status(405).json({ error: true, message: "Invalid request method" });
+  }
+}
---
+++ pages/api/logout.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    try {
+      res.setHeader(
+        "Set-Cookie",
+        `token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; Secure`
+      );
+
+      res
+        .status(200)
+        .json({ error: false, message: "Logged out successfully" });
+    } catch (error) {
+      res.status(500).json({ error: true, message: "Failed to log out" });
+    }
+  } else {
+    res.status(405).json({ error: true, message: "Invalid request method" });
+  }
+}
