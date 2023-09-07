5ef100e4df2e7dc7f8dc5ceb8ede415462542ae2
hypulse <sjinsilval28@naver.com>
2023-09-07 15:30:43 +0900

    Use getStaticPaths and getServerSideProps(and it will use getServerSideProps when there's no matching id)

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,14 +1,26 @@
-import { GetServerSideProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import MarkedViewer from "@/components/MarkedViewer";
 import { PostTypeGet } from "@/types/post";
 import { getPost } from "@/utils/apis/posts";
 import sharePost from "@/utils/sharePost";
+import getAllPostIds from "@/server/getAllPostIds";
+import connectToDatabase from "@/server/connectToDatabase";
 
-type PageProps = {
-  data: PostTypeGet;
+export const getStaticPaths: GetStaticPaths = async () => {
+  await connectToDatabase();
+  const posts = await getAllPostIds();
+
+  const paths = posts.map((postId) => ({
+    params: { id: postId.toString() },
+  }));
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
 };
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getStaticProps: GetStaticProps = async (context) => {
   const postId = context.params?.id;
 
   if (typeof postId !== "string" || !postId) {
@@ -24,7 +36,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   return { props: { data } };
 };
 
-export default function ({ data }: PageProps) {
+export default function ({ data }: { data: PostTypeGet }) {
   const { title, createdAt, summary, content } = data;
   const createdAtDate = new Date(createdAt);
 
