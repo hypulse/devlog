3e36e8f698becd096450f93852de0e1ac6519628
hypulse <sjinsilval28@naver.com>
2023-09-07 10:30:44 +0900

    add user verifying on the posts route

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -1,5 +1,6 @@
 import Post from "@/server/Models/Post";
 import connectToDatabase from "@/server/connectToDatabase";
+import verifyUser from "@/server/verifyUser";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
@@ -8,12 +9,18 @@ export default async function handler(
 ) {
   await connectToDatabase();
 
+  const token = req.cookies.token;
+
   let { page = 1, limit = 10, state = "active" } = req.query;
   limit = Math.min(Number(limit), 50);
 
   switch (req.method) {
     case "GET":
       try {
+        if (!["active", "snippet"].includes(state as string)) {
+          verifyUser(token);
+        }
+
         const skipValue = (Number(page) - 1) * Number(limit);
         const queryOptions = {
           state: state as string,
@@ -39,6 +46,8 @@ export default async function handler(
 
     case "POST":
       try {
+        verifyUser(token);
+
         const post = await Post.create(req.body);
         res.status(201).json({ error: false, data: post });
       } catch (error) {
