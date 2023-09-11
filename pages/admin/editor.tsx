7bdfa9c859512c61f978e28b26c1a5703298b94a
hypulse <sjinsilval28@naver.com>
2023-09-11 10:23:15 +0900

    컴포넌트 분리;임시저장 UI 완성

--- pages/admin/editor.tsx
+++ pages/admin/editor.tsx
@@ -1,4 +1,5 @@
 import Button from "@/components/Button";
+import IconButton from "@/components/IconButton";
 import verifyUser from "@/server/verifyUser";
 import { PostState, PostTypePost } from "@/types/post";
 import { createPost, getPost, updatePost } from "@/utils/apis/posts";
@@ -6,7 +7,7 @@ import parseContent from "@/utils/parseContent";
 import { Marked } from "marked";
 import { GetServerSidePropsContext } from "next";
 import { useRouter } from "next/router";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, SVGProps } from "react";
 import MdEditor from "react-markdown-editor-lite";
 import "react-markdown-editor-lite/lib/index.css";
 
@@ -35,6 +36,8 @@ export default function Page() {
   const [state, setState] = useState<PostState>("draft");
   const [postId, setPostId] = useState<string | null>(null);
   const mdEditor = useRef<MdEditor>(null);
+  const [showSavedPosts, setShowSavedPosts] = useState(false);
+  const [savedPosts, setSavedPosts] = useState([]);
 
   useEffect(() => {
     if (isReady && query.id) {
@@ -54,8 +57,13 @@ export default function Page() {
 
     window.addEventListener("keydown", handleKeyDown);
 
+    window.onbeforeunload = () => {
+      return "Are you sure you want to leave? Changes you made may not be saved.";
+    };
+
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
+      window.onbeforeunload = null;
     };
   }, []);
 
@@ -79,6 +87,13 @@ export default function Page() {
     localStorage.setItem("auto_saved_posts", JSON.stringify(savedPosts));
 
     alert("Temporarily saved at " + currentTime);
+  }
+
+  function handleShowSavedPosts() {
+    const savedPostsString = localStorage.getItem("auto_saved_posts");
+    const savedPosts = savedPostsString ? JSON.parse(savedPostsString) : [];
+    setSavedPosts(savedPosts);
+    setShowSavedPosts(true);
   }
 
   function getMdValue() {
@@ -146,67 +161,159 @@ export default function Page() {
   };
 
   return (
-    <div className="space-y-elementGap">
+    <div className="flex flex-col gap-y-elementGap">
+      <div className="flex items-center justify-end gap-x-colGap mb-rowGap">
+        <FileInput handleFileChange={handleFileChange} />
+        <button
+          className="border border-border flex items-center gap-x-xsGap px-tagPaddingX py-tagPaddingY rounded cursor-pointer"
+          onClick={handleShowSavedPosts}
+        >
+          임시저장 글
+        </button>
+      </div>
       <MdEditor
         ref={mdEditor}
         style={{ height: "500px" }}
         renderHTML={(text) => new Marked().parse(text) as string}
       />
-      <div className="flex items-center justify-between flex-wrap gap-y-rowGap">
-        <fieldset>
-          <legend className="text-caption text-textSecondary mb-xsGap">
-            Select state
-          </legend>
-          <div className="flex space-x-colGap">
-            <label className="flex space-x-xsGap">
-              <input
-                type="radio"
-                value="draft"
-                checked={state === "draft"}
-                onChange={() => setState("draft")}
-              />
-              <span>Draft</span>
-            </label>
-            <label className="flex space-x-xsGap items-center">
-              <input
-                type="radio"
-                value="active"
-                checked={state === "active"}
-                onChange={() => setState("active")}
-              />
-              <span>Article</span>
-            </label>
-            <label className="flex space-x-xsGap items-center">
-              <input
-                type="radio"
-                value="snippet"
-                checked={state === "snippet"}
-                onChange={() => setState("snippet")}
-              />
-              <span>Snippet</span>
-            </label>
-          </div>
-        </fieldset>
-        <div>
-          <input
-            type="file"
-            id="fileInput"
-            onChange={handleFileChange}
-            style={{
-              display: "none",
-            }}
-          ></input>
-          <label
-            htmlFor="fileInput"
-            className="border border-border flex items-center gap-x-xsGap px-tagPaddingX py-tagPaddingY rounded cursor-pointer"
-          >
-            파일 선택
-          </label>
-        </div>
-      </div>
-      <Button onClick={handleSubmit} className="bg-primary w-full">
+      <SelectPostState state={state} setState={setState} />
+      <Button onClick={handleSubmit} className="bg-primary">
         {postId ? "Update" : "Create"}
       </Button>
+      <SavedPosts
+        showSavedPosts={showSavedPosts}
+        setShowSavedPosts={setShowSavedPosts}
+        savedPosts={savedPosts}
+        setMdValue={setMdValue}
+      />
     </div>
+  );
+}
+
+const SavedPosts = ({
+  showSavedPosts,
+  setShowSavedPosts,
+  savedPosts,
+  setMdValue,
+}: any) => {
+  const handleChooseSavedPost = (post: any) => {
+    setMdValue(post.content);
+    setShowSavedPosts(false);
+  };
+
+  const handleBackgroundClick = (event: any) => {
+    if (event.target === event.currentTarget) {
+      setShowSavedPosts(false);
+    }
+  };
+
+  if (!showSavedPosts) return null;
+
+  return (
+    <div
+      className="fixed top-0 left-0 w-full h-full bg-background/50 backdrop-blur flex justify-center items-center"
+      onClick={handleBackgroundClick}
+    >
+      <ul className="bg-card rounded py-sectionGap pb-extraGap overflow-y-scroll list-none relative shadow">
+        <IconButton
+          className="absolute top-cellPadding right-cellPadding"
+          onClick={() => setShowSavedPosts(false)}
+        >
+          <RiCloseFill />
+        </IconButton>
+        {savedPosts
+          .reverse()
+          .map((post: { id: string; timestamp: string; content: string }) => (
+            <li
+              key={post.id}
+              onClick={() => {
+                handleChooseSavedPost(post);
+              }}
+              className="px-buttonPaddingX py-buttonPaddingY border-b border-border cursor-pointer last:border-b-0 hover:bg-border"
+            >
+              {`${new Date(
+                post.timestamp
+              ).toLocaleString()} - ${post.content.slice(0, 20)}...`}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+const FileInput = ({ handleFileChange }: any) => {
+  return (
+    <div>
+      <input
+        type="file"
+        id="fileInput"
+        onChange={handleFileChange}
+        style={{
+          display: "none",
+        }}
+      ></input>
+      <label
+        htmlFor="fileInput"
+        className="border border-border flex items-center gap-x-xsGap px-tagPaddingX py-tagPaddingY rounded cursor-pointer"
+      >
+        파일 선택
+      </label>
+    </div>
+  );
+};
+
+const SelectPostState = ({ state, setState }: any) => {
+  return (
+    <fieldset>
+      <legend className="text-caption text-textSecondary mb-xsGap">
+        Select state
+      </legend>
+      <div className="flex space-x-colGap">
+        <label className="flex space-x-xsGap">
+          <input
+            type="radio"
+            value="draft"
+            checked={state === "draft"}
+            onChange={() => setState("draft")}
+          />
+          <span>Draft</span>
+        </label>
+        <label className="flex space-x-xsGap items-center">
+          <input
+            type="radio"
+            value="active"
+            checked={state === "active"}
+            onChange={() => setState("active")}
+          />
+          <span>Article</span>
+        </label>
+        <label className="flex space-x-xsGap items-center">
+          <input
+            type="radio"
+            value="snippet"
+            checked={state === "snippet"}
+            onChange={() => setState("snippet")}
+          />
+          <span>Snippet</span>
+        </label>
+      </div>
+    </fieldset>
+  );
+};
+
+function RiCloseFill(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="m12 10.586l4.95-4.95l1.415 1.415l-4.95 4.95l4.95 4.95l-1.415 1.414l-4.95-4.95l-4.95 4.95l-1.413-1.415l4.95-4.95l-4.95-4.95L7.05 5.638l4.95 4.95Z"
+      ></path>
+    </svg>
   );
 }
