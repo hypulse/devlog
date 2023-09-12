dc38f9c44eb9e3b29fc786f89f963cc0ec4ac2b8
hypulse <sjinsilval28@naver.com>
2023-09-12 11:05:14 +0900

    Remove static builds(with this I need to seperate docker mongodb from the context)

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,4 +1,8 @@
-import { GetStaticPaths, GetStaticProps } from "next";
+import {
+  GetServerSidePropsContext,
+  GetStaticPaths,
+  GetStaticProps,
+} from "next";
 import MarkedViewer from "@/components/MarkedViewer";
 import { PostTypeGet } from "@/types/post";
 import sharePost from "@/utils/sharePost";
@@ -6,21 +10,37 @@ import getAllPostIds from "@/server/getAllPostIds";
 import connectToDatabase from "@/server/connectToDatabase";
 import fetchAPI from "@/utils/fetchAPI";
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  await connectToDatabase();
-  const posts = await getAllPostIds();
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   await connectToDatabase();
+//   const posts = await getAllPostIds();
 
-  const paths = posts.map((postId) => ({
-    params: { id: postId.toString() },
-  }));
+//   const paths = posts.map((postId) => ({
+//     params: { id: postId.toString() },
+//   }));
 
-  return {
-    paths,
-    fallback: "blocking",
-  };
-};
+//   return {
+//     paths,
+//     fallback: "blocking",
+//   };
+// };
 
-export const getStaticProps: GetStaticProps = async (context) => {
+// export const getStaticProps: GetStaticProps = async (context) => {
+//   const postId = context.params?.id;
+
+//   if (typeof postId !== "string" || !postId) {
+//     return { notFound: true };
+//   }
+
+//   const { error, data } = await fetchAPI<PostTypeGet>(`/posts/${postId}`);
+
+//   if (error || !data) {
+//     return { notFound: true };
+//   }
+
+//   return { props: { data } };
+// };
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
   const postId = context.params?.id;
 
   if (typeof postId !== "string" || !postId) {
@@ -34,7 +54,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
   }
 
   return { props: { data } };
-};
+}
 
 export default function Page({ data }: { data: PostTypeGet }) {
   const { title, createdAt, summary, content } = data;
