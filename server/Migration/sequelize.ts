1c0498eff7c22dc5b5d6e761217f632e7b243f02
hypulse <sjinsilval28@naver.com>
2023-11-14 18:02:29 +0900

    Update database connection configuration.

--- server/Migration/sequelize.ts
+++ server/Migration/sequelize.ts
@@ -1,12 +1,12 @@
 import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(
-  process.env.MARIADB_DATABASE!,
-  process.env.MARIADB_USER!,
-  process.env.MARIADB_PASSWORD,
+  process.env.DB_NAME!,
+  process.env.DB_USER!,
+  process.env.DB_PASSWORD!,
   {
-    host: "mariadb",
     dialect: "mariadb",
+    host: process.env.DB_HOST,
   }
 );
 
