cf70ed905443ce2dafe7511685f970c098c5e7cf
hypulse <sjinsilval28@naver.com>
2023-11-15 17:18:23 +0900

    Add migration for creating Posts table

--- migrations/20231115074131-create-posts-3.js
+++ migrations/20231115074131-create-posts-3.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("posts", {
+    await queryInterface.createTable("Posts", {
       id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
