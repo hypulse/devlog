690ca3e98ed6db7dbeb806354a6f5971809076b2
hypulse <sjinsilval28@naver.com>
2023-11-17 16:06:12 +0900

    Refactor Post update API endpoint.

--- pages/api/posts/[id].ts
+++ pages/api/posts/[id].ts
@@ -43,18 +43,24 @@ export default async function handler(
       try {
         verifyUser(token);
 
-        const post = await Post.update(req.body, {
+        const [updateCount] = await Post.update(req.body, {
           where: { id: id },
-          returning: true,
         });
 
-        if (!post[1][0]) {
+        if (updateCount === 0) {
           return res
             .status(404)
             .json({ error: true, message: "Post not found" });
         }
 
-        res.status(200).json({ error: false, data: post[1][0] });
+        const updatedPost = await Post.findByPk(id as string);
+        if (!updatedPost) {
+          return res
+            .status(404)
+            .json({ error: true, message: "Post not found after update" });
+        }
+
+        res.status(200).json({ error: false, data: updatedPost });
       } catch (error) {
         res.status(400).json({ error: true, message: "Failed to update post" });
       }
