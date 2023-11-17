104ccc5f70e1a9255f4d1bf2d26b2fc860532cee
hypulse <sjinsilval28@naver.com>
2023-11-17 14:25:25 +0900

    Add CORS headers to API routes and remove unused middleware.

--- middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse, type NextRequest } from "next/server";
-
-export async function middleware(request: NextRequest) {
-  // let token = request.cookies.get("token")?.value || "";
-  // const response = NextResponse.next();
-  // response.cookies.set("token", token);
-  // return response;
-}
