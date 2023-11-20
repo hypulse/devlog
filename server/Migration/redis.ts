5824b7623e1e44bb65dff7e2304cbafbcd55c369
hypulse <sjinsilval28@naver.com>
2023-11-20 09:35:47 +0900

    Update Redis URI to use environment variable

--- server/Migration/redis.ts
+++ server/Migration/redis.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@redis/client";
 
 const redisClient = createClient({
-  url: "redis://localhost:6379",
+  url: `redis://${process.env.DB_REDIS_HOST}:6379`,
 });
 
 export default redisClient;
