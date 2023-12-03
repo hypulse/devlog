df78e4b0741518b0fbe56551fb967ae80a0b6973
hypulse <sjinsilval28@naver.com>
2023-12-04 01:11:10 +0900

    Add backup functionality to save posts as markdown files

--- backup.js
+++ backup.js
@@ -1,20 +1,59 @@
 const { exec } = require("child_process");
+const fs = require("fs");
+const path = require("path");
 
-async function downloadActivePosts() {}
-async function downloadSnippets() {}
+const saveDir = path.join(__dirname, "backup");
+
+if (!fs.existsSync(saveDir)) {
+  fs.mkdirSync(saveDir, { recursive: true });
+}
+
+async function downloadPosts() {
+  const response = await fetch("/api/backup");
+  const { error, data } = await response.json();
+  if (error) throw new Error(error);
+  return data;
+}
+
+async function savePostsAsMarkdown(data) {
+  const articles = data.articles;
+  const snippets = data.snippets;
+  articles.forEach((article) => {
+    savePostAsMarkdown(article);
+  });
+  snippets.forEach((snippet) => {
+    savePostAsMarkdown(snippet);
+  });
+}
+
+function savePostAsMarkdown(post) {
+  return new Promise((resolve, reject) => {
+    const fileName = `${post.title}-${post.createdAt}.md`;
+    const filePath = path.join(saveDir, fileName);
+
+    fs.writeFile(filePath, post.content, (err) => {
+      if (err) {
+        reject(new Error(err));
+      } else {
+        console.log("File saved: ", fileName);
+        resolve();
+      }
+    });
+  });
+}
 
 function uploadToGithub() {
   exec("", (error, stdout, stderr) => {
-    if (error) {
-      console.error(`실행 오류: ${error}`);
-      return;
-    }
+    if (error) throw new Error(error);
     console.log(`stdout: ${stdout}`);
     console.error(`stderr: ${stderr}`);
   });
 }
 
-Promise.all([downloadActivePosts(), downloadSnippets()])
+downloadPosts()
+  .then(async (data) => {
+    await savePostsAsMarkdown(data);
+  })
   .then(() => {
     uploadToGithub();
   })
---
+++ pages/api/backup.ts
@@ -0,0 +1,47 @@
+import Post from "@/server/Migration/Models/Post";
+import connectToDatabase from "@/server/Migration/connectToDatabase";
+import verifyUser from "@/server/verifyUser";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await connectToDatabase();
+
+  const token = req.cookies.token;
+
+  switch (req.method) {
+    case "GET":
+      try {
+        verifyUser(token);
+        const articles = await Post.findAll({
+          where: {
+            state: "active",
+          },
+        });
+        const snippets = await Post.findAll({
+          where: {
+            state: "snippet",
+          },
+        });
+        res.status(200).json({
+          error: false,
+          data: {
+            articles: articles,
+            snippets: snippets,
+          },
+        });
+      } catch (error) {
+        console.log(error);
+        res
+          .status(400)
+          .json({ error: true, message: "Failed to retrieve posts" });
+      }
+      break;
+
+    default:
+      res.status(405).json({ error: true, message: "Invalid request method" });
+      break;
+  }
+}
