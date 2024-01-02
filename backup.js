bad345f7793a329b58a374cc49a26432f57e4e1f
hypulse <sjinsilval28@naver.com>
2024-01-02 15:51:02 +0900

    Add backup folder deletion and cleanup logic

--- backup.js
+++ backup.js
@@ -1,10 +1,43 @@
 const fs = require("fs");
 const path = require("path");
 
-const saveDir = path.join(__dirname, "backup");
+// 현재 날짜를 yyyy-mm-dd 형식으로 포맷하는 함수
+function formatDate(date) {
+  return date.toISOString().split("T")[0];
+}
 
+const today = formatDate(new Date());
+const saveDir = path.join(__dirname, "backup", today);
+
+// 오늘 날짜 폴더가 없으면 생성
 if (!fs.existsSync(saveDir)) {
   fs.mkdirSync(saveDir, { recursive: true });
+}
+
+// 오늘 날짜가 아닌 폴더 삭제
+function deleteOldFolders(baseFolder) {
+  fs.readdir(baseFolder, (err, folders) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    folders.forEach((folder) => {
+      if (folder !== today) {
+        fs.rm(
+          path.join(baseFolder, folder),
+          { recursive: true, force: true },
+          (err) => {
+            if (err) {
+              console.error(err);
+            } else {
+              console.log(`Deleted folder: ${folder}`);
+            }
+          }
+        );
+      }
+    });
+  });
 }
 
 async function downloadPosts() {
@@ -44,6 +77,7 @@ function savePostAsMarkdown(post) {
 downloadPosts()
   .then(async (data) => {
     await savePostsAsMarkdown(data);
+    deleteOldFolders(path.join(__dirname, "backup"));
   })
   .catch((err) => {
     console.log(err);
